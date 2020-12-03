19d706ab1fd0c63d795b1b7251b5f30dbc35cf55
Yan <Yan@DESKTOP-L2JQ9CJ>
2020-12-03 14:49:54 +0800

    添加项目文件。

---
+++ resource.h
@@ -0,0 +1,22 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCTest.rc 使用
+//
+#define IDD_MFCTEST_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT_PID                    1000
+#define IDC_EDIT_DLLPath                1001
+#define IDC_EDIT_Path                   1001
+#define IDC_BUTTON_Inject               1002
+#define IDC_BUTTON_SelectFile           1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
